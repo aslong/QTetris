81f427b78d5ec91021df85d67349943348982e3c
Andrew Long <aslong87@gmail.com>
2012-04-04 21:53:38 -0700

    Refactor SBlock to use angle and fill using color.

--- SBlock.cpp
+++ SBlock.cpp
@@ -18,7 +18,7 @@ SBlock::SBlock(QPointF point)
 
 QRectF SBlock::boundingRect() const
 {
-    if (rotateBlock)
+    if (angle == 90 || angle == 270)
     {
         return QRectF(x(), y(), BLOCK_HEIGHT, BLOCK_WIDTH);
     }
@@ -30,20 +30,21 @@ QPainterPath SBlock::shape() const
 {
     QPainterPath path;
 
-    if (rotateBlock)
+    if (angle == 0 || angle == 180)
     {
-        path.addRect(x(), y(), BLOCK_SIZE - 1, BLOCK_SIZE);
-        path.addRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE - 1);
-        path.addRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE - 1, BLOCK_SIZE);
-        path.addRect(x() + BLOCK_SIZE, y() + (BLOCK_SIZE * 2), BLOCK_SIZE - 1, BLOCK_SIZE);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + (BLOCK_SIZE * 2) + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
-    else
+    else 
     {
-        path.addRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE);
-        path.addRect(x() + (BLOCK_SIZE * 2), y(), BLOCK_SIZE - 1, BLOCK_SIZE - 1);
-        path.addRect(x(), y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
-        path.addRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE - 1, BLOCK_SIZE);
+        path.addRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
+
     return path;
 }
 
@@ -52,5 +53,28 @@ void SBlock::paint(QPainter *painter,
                     const QStyleOptionGraphicsItem *option,
                     QWidget *widget)
 {
-    painter->drawPath(shape());
+    if (angle == 0 || angle == 180)
+    {
+        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + (BLOCK_SIZE * 2), y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + (BLOCK_SIZE * 2) + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+    }
+    else
+    {
+        painter->drawRect(x(), y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + (BLOCK_SIZE * 2), BLOCK_SIZE, BLOCK_SIZE);
+
+        painter->fillRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+    }
 }
