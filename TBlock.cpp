0232aaaa7fc8c6444e2e5d9f32dcfa416b6868b2
andrew long <andrew.long@palm.com>
2012-04-09 21:44:57 -0700

    Draw and fill TBlock using rects

--- TBlock.cpp
+++ TBlock.cpp
@@ -44,23 +44,31 @@ QPainterPath TBlock::shape() const
 
     if (angle == 0)
     {
-        path.addRect(x() + BLOCK_SIZE + 1, y(), BLOCK_SIZE - 2, BLOCK_SIZE - 1);
-        path.addRect(x(), y() + BLOCK_SIZE + 1, BLOCK_WIDTH, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + (BLOCK_SIZE * 2) + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
     else if (angle == 90)
     {
-        path.addRect(x(), y(), BLOCK_SIZE - 1, BLOCK_WIDTH);
-        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 2);
+        path.addRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
     else if (angle == 180)
     {
-        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 1);
-        path.addRect(x() + 1, y(), BLOCK_WIDTH, BLOCK_SIZE - 1);
+        path.addRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + (BLOCK_SIZE * 2) + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
     else if (angle == 270)
     {
-        path.addRect(x() + BLOCK_SIZE + 1, y(), BLOCK_SIZE - 1, BLOCK_WIDTH);
-        path.addRect(x(), y() + BLOCK_SIZE + 1, BLOCK_SIZE, BLOCK_SIZE - 2);
+        path.addRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+        path.addRect(x() + BLOCK_SIZE + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
     }
 
     return path;
@@ -73,64 +81,50 @@ void TBlock::paint(QPainter *painter,
 {
     if (angle == 0)
     { 
-        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE); 
-        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_WIDTH, BLOCK_SIZE);
-        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, FILL_COLOR); 
-        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_WIDTH - 2, BLOCK_SIZE - 2, FILL_COLOR);
+        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + (BLOCK_SIZE * 2), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 
-        for (int i = 1; i < BLOCK_WIDTH / BLOCK_SIZE; i++)
-        {
-            int newStartX =  x() + (BLOCK_SIZE * i);
-            int newStartY =  y() + BLOCK_SIZE;
-            int newEndX =  x() + (BLOCK_SIZE * i);
-            int newEndY =  y() + BLOCK_HEIGHT;
-            painter->drawLine(newStartX, newStartY, newEndX, newEndY);
-        }
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + (BLOCK_SIZE * 2) + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
     }
     else if (angle == 90) 
     {
-        painter->drawRect(x(), y(), BLOCK_SIZE, BLOCK_WIDTH); 
-        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE); 
-        painter->fillRect(x() + 1, y() + 1, BLOCK_SIZE - 2, BLOCK_WIDTH - 2, FILL_COLOR); 
-        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, FILL_COLOR); 
+        painter->drawRect(x(), y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x(), y() + (BLOCK_SIZE * 2), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 
-        for (int i = 1; i < BLOCK_WIDTH / BLOCK_SIZE; i++)
-        {
-            int newStartX = x();
-            int newStartY = y() + (BLOCK_SIZE * i);
-            int newEndX = x() + BLOCK_SIZE;
-            int newEndY = y() + (BLOCK_SIZE * i);
-            painter->drawLine(newStartX, newStartY, newEndX, newEndY);
-        }
+        painter->fillRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
     }
     else if (angle == 180)
     {
-        painter->drawRect(x(), y(), BLOCK_WIDTH, BLOCK_SIZE); 
+        painter->drawRect(x(), y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + (BLOCK_SIZE * 2), y(), BLOCK_SIZE, BLOCK_SIZE);
         painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-        painter->fillRect(x(), y(), BLOCK_WIDTH - 2, BLOCK_SIZE - 2, FILL_COLOR); 
-        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, FILL_COLOR);
-        for (int i = 1; i < BLOCK_WIDTH / BLOCK_SIZE; i++)
-        {
-            int newStartX =  x() + (BLOCK_SIZE * i);
-            int newStartY =  y();
-            int newEndX =  x() + (BLOCK_SIZE * i);
-            int newEndY =  y() + BLOCK_SIZE;
-            painter->drawLine(newStartX, newStartY, newEndX, newEndY);
-        }
+
+        painter->fillRect(x() + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + (BLOCK_SIZE * 2) + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
     }
     else if (angle == 270)
     {
-        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_WIDTH); 
-        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE); 
-        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 2, BLOCK_WIDTH - 2, FILL_COLOR); 
-        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, FILL_COLOR); 
-        for (int i = 1; i < BLOCK_WIDTH / BLOCK_SIZE; i++)
-        {
-            int newStartX = x() + BLOCK_SIZE;
-            int newStartY = y() + (BLOCK_SIZE * i);
-            int newEndX = x() + BLOCK_HEIGHT;
-            int newEndY = y() + (BLOCK_SIZE * i);
-            painter->drawLine(newStartX, newStartY, newEndX, newEndY);
-        }
+        painter->drawRect(x(), y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y(), BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        painter->drawRect(x() + BLOCK_SIZE, y() + (BLOCK_SIZE * 2), BLOCK_SIZE, BLOCK_SIZE);
+        
+        painter->fillRect(x() + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + BLOCK_SIZE + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
+        painter->fillRect(x() + BLOCK_SIZE + 1, y() + (BLOCK_SIZE * 2) + 1, BLOCK_SIZE - 1, BLOCK_SIZE - 1, FILL_COLOR);
     }
 }
